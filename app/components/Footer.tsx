2da1dc839de04dc6d375732e3186eabbf1d5a9a5
Francesco Scanferla <info@francescoscanferla.it>
2026-07-07 23:04:14 +0200

    Gestita versione con variabile d'ambiente

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export const Footer: React.FC = () => {
+    const currentVersion = process.env.NEXT_PUBLIC_APP_VERSION || 'Local';
     return (
         <footer className="bg-ctp-overlay p-4 mt-8 flex justify-between items-center text-sm">
             <p className="text-ctp-subtext">
                 © {new Date().getFullYear()} B.Y.O.B. Stats
             </p>
             <div className="px-3 py-1.5 text-xs font-mono text-ctp-muted">
-                Versione: {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || 'Sviluppo Locale'}
+                Versione: {currentVersion}
             </div>
         </footer>
     );
